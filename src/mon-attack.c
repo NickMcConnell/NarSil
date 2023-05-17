NickMcConnell/Beleriand

commit f7f0e27a2c984fe679bbef302eda0e2803a6aed4

Plug memory leaks detected when running the tutorial

src/mon-attack.c
@@ -356,6 +356,7 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 	int net_dam = 0;
 
 	char *act = NULL;
+	bool act_allocated = false;
 
 	/* Get the monster name (or "it") */
 	monster_desc(m_name, sizeof(m_name), mon, MDESC_STANDARD);
@@ -401,6 +402,7 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 
 		/* Describe the attack method */
 		act = monster_blow_method_action(method, -1);
+		act_allocated = true;
 		do_cut = method->cut;
 		do_stun = method->stun;
 		do_prt = method->prt;
@@ -408,7 +410,11 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 
 		/* Special case */
 		if (streq(method->name, "HIT") && streq(effect->name, "BATTER")) {
+			if (act_allocated) {
+				string_free(act);
+			}
 			act = (char *) "batters you";
+			act_allocated = false;
 		}
 
 		/* Hack -- assume all attacks are obvious */
@@ -449,6 +455,9 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 			}
                 
 			msgt(sound_msg, "%s %s%s", m_name, act, punctuation);
+			if (act_allocated) {
+				string_free(act);
+			}
 		}
 
 		/* Perform the actual effect. */

src/cmd-wizard.c
@@ -33,6 +33,7 @@
 #include "obj-pile.h"
 #include "obj-tval.h"
 #include "obj-util.h"
+#include "player-abilities.h"
 #include "player-calcs.h"
 #include "player-timed.h"
 #include "player-util.h"
@@ -1666,11 +1667,13 @@ void do_cmd_wiz_play_item(struct command *cmd)
 			struct object *prev = obj->prev;
 			struct object *next = obj->next;
 
-			/* Free slays, brands, and curses by hand. */
+			/* Free slays, brands, and abilities by hand. */
 			mem_free(obj->slays);
 			obj->slays = NULL;
 			mem_free(obj->brands);
 			obj->brands = NULL;
+			release_ability_list(obj->abilities);
+			obj->abilities = NULL;
 
 			object_copy(obj, orig_obj);
 			obj->prev = prev;
@@ -2118,11 +2121,15 @@ void do_cmd_wiz_reroll_item(struct command *cmd)
 		bitflag notice = obj->notice;
 		bool marked = obj->marked;
 
-		/* Free slays, brands, and curses on the old object by hand. */
+		/*
+		 * Free slays, brands, and abilities on the old object by hand.
+		 */
 		mem_free(obj->slays);
 		obj->slays = NULL;
 		mem_free(obj->brands);
 		obj->brands = NULL;
+		release_ability_list(obj->abilities);
+		obj->abilities = NULL;
 
 		/* Copy over; pile information needs to be restored. */
 		object_copy(obj, new);

src/game-world.c
@@ -321,7 +321,7 @@ void update_flow(struct chunk *c, struct flow *flow, struct monster *mon)
 	struct loc next = flow->centre;
 	int y, x, d;
 	int value = 0;
-    struct queue *queue = q_new(c->height * c->width);
+	struct queue *queue = q_new(c->height * c->width);
 
 	/* Set all the grids to maximum */
 	for (y = 1; y < c->height - 1; y++) {
@@ -388,6 +388,8 @@ void update_flow(struct chunk *c, struct flow *flow, struct monster *mon)
 		}
 		value++;
 	}
+
+	q_free(queue);
 }
 
 /**

src/init.c
@@ -994,6 +994,9 @@ static void cleanup_feat(void) {
 		string_free(f_info[idx].look_in_preposition);
 		string_free(f_info[idx].look_prefix);
 		string_free(f_info[idx].confused_msg);
+		string_free(f_info[idx].str_msg);
+		string_free(f_info[idx].fail_msg);
+		string_free(f_info[idx].dig_msg);
 		string_free(f_info[idx].die_msg);
 		string_free(f_info[idx].hurt_msg);
 		string_free(f_info[idx].run_msg);
@@ -1533,6 +1536,7 @@ static void cleanup_race(void)
 			item = item_next;
 		}
 		string_free((char *)r->name);
+		string_free((char *)r->desc);
 		mem_free(r);
 		r = next;
 	}
@@ -1685,7 +1689,10 @@ static void cleanup_house(void)
 
 	while (h) {
 		next = h->next;
-		mem_free((char *)h->name);
+		string_free((char *)h->name);
+		string_free((char *)h->alt_name);
+		string_free((char *)h->short_name);
+		string_free((char *)h->desc);
 		mem_free(h);
 		h = next;
 	}
@@ -1877,6 +1884,7 @@ static void cleanup_flavor(void)
 	f = flavors;
 	while(f) {
 		next = f->next;
+		string_free(f->text);
 		mem_free(f);
 		f = next;
 	}

src/mon-make.c
@@ -582,6 +582,9 @@ void wipe_mon_list(struct chunk *c, struct player *p)
 		/* Monster is gone from square */
 		square_set_mon(c, mon->grid, 0);
 
+		/* Free flow */
+		flow_free(c, &mon->flow);
+
 		/* Wipe the Monster */
 		memset(mon, 0, sizeof(struct monster));
 	}

src/obj-chest.c
@@ -232,6 +232,7 @@ static void cleanup_chest_trap(void)
 		string_free(trap->name);
 		string_free(trap->msg);
 		string_free(trap->msg_save);
+		string_free(trap->msg_bad);
 		string_free(trap->msg_death);
 		free_effect(trap->effect);
 		trap = trap->next;

src/obj-init.c
@@ -683,6 +683,8 @@ static void cleanup_object_base(void)
 	int idx;
 	for (idx = 0; idx < TV_MAX; idx++) {
 		string_free(kb_info[idx].name);
+		mem_free(kb_info[idx].smith_slays);
+		mem_free(kb_info[idx].smith_brands);
 	}
 	mem_free(kb_info);
 }
@@ -1579,6 +1581,7 @@ static void cleanup_object(void)
 		string_free(kind->effect_msg);
 		mem_free(kind->brands);
 		mem_free(kind->slays);
+		release_ability_list(kind->abilities);
 		free_effect(kind->effect);
 		free_effect(kind->thrown_effect);
 		mem_free(kind->alloc);
@@ -2123,6 +2126,7 @@ static void cleanup_ego(void)
 		string_free(ego->name);
 		mem_free(ego->brands);
 		mem_free(ego->slays);
+		release_ability_list(ego->abilities);
 
 		poss = ego->poss_items;
 		while (poss) {
@@ -2491,6 +2495,7 @@ static void cleanup_artifact(void)
 		string_free(art->text);
 		mem_free(art->brands);
 		mem_free(art->slays);
+		release_ability_list(art->abilities);
 	}
 	mem_free(a_info);
 	mem_free(aup_info);

src/obj-pile.c
@@ -37,6 +37,7 @@
 #include "obj-slays.h"
 #include "obj-tval.h"
 #include "obj-util.h"
+#include "player-abilities.h"
 #include "player-calcs.h"
 #include "player-history.h"
 #include "player-util.h"
@@ -293,6 +294,7 @@ void object_free(struct object *obj)
 {
 	mem_free(obj->slays);
 	mem_free(obj->brands);
+	release_ability_list(obj->abilities);
 	mem_free(obj);
 }
 
@@ -580,6 +582,7 @@ void object_wipe(struct object *obj)
 	/* Free slays and brands */
 	mem_free(obj->slays);
 	mem_free(obj->brands);
+	release_ability_list(obj->abilities);
 
 	/* Wipe the structure */
 	memset(obj, 0, sizeof(*obj));
@@ -601,6 +604,9 @@ void object_copy(struct object *dest, const struct object *src)
 	if (src->brands) {
 		dest->brands = mem_zalloc(z_info->brand_max * sizeof(bool));
 		memcpy(dest->brands, src->brands, z_info->brand_max * sizeof(bool));
+	}
+	if (src->abilities) {
+		dest->abilities = copy_ability_list(src->abilities);
 	}
 
 	/* Detach from any pile */

NickMcConnell/NarSil

commit ef06883c6a2c63859d5a5a47d7857d73d33b60d0

Avoid crash due to erroneous free in make_attack_normal()
Potential for crash was introduced by f7f0e27a2c984fe679bbef302eda0e2803a6aed4 .  Resolves https://github.com/NickMcConnell/NarSil/issues/192 .

src/mon-attack.c
@@ -450,7 +450,11 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 				/* Remember that the monster can do this */
 				if (monster_is_visible(mon)) {
 					rf_on(lore->flags, RF_CHARGE);
+					if (act_allocated) {
+						string_free(act);
+					}
 					act = (char *) "charges you";
+					act_allocated = false;
                 }
 			}
                 