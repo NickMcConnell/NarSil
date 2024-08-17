8e9c163120efea73c7ed88c73cece2d9751f5f14
Nick McConnell <nckmccnnll@gmail.com>
2024-08-18 07:49:17 +1000

    Player cave used, compiles and runs, ID probably has problems

--- src/tests/parse/f-info.c
+++ src/tests/parse/f-info.c
@@ -6,8 +6,12 @@
 #include "cave.h"
 #include "init.h"
 #include "monster.h"
+#include "player.h"
+#include "z-form.h"
+#ifndef WINDOWS
 #include <locale.h>
 #include <langinfo.h>
+#endif
 
 
 int setup_tests(void **state) {
@@ -22,18 +26,14 @@ int teardown_tests(void *state) {
 	string_free(f->look_in_preposition);
 	string_free(f->look_prefix);
 	string_free(f->confused_msg);
-	string_free(f->str_msg);
-	string_free(f->fail_msg);
-	string_free(f->dig_msg);
 	string_free(f->die_msg);
 	string_free(f->hurt_msg);
 	string_free(f->run_msg);
 	string_free(f->walk_msg);
-	string_free(f->mimic);
 	string_free(f->desc);
 	string_free(f->name);
-	mem_free(f);
-	parser_destroy(p);
+	mem_free(f_info);
+	parser_destroy(state);
 	return 0;
 }
 
@@ -43,15 +43,17 @@ static int test_missing_header_record0(void *state) {
 	enum parser_error r;
 
 	null(f);
+	r = parser_parse(p, "name:Test Feature");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
 	r = parser_parse(p, "graphics: :w");
 	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
 	r = parser_parse(p, "priority:2");
 	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
-	r = parser_parse(p, "mimic:granite wall");
+	r = parser_parse(p, "mimic:GRANITE");
 	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
 	r = parser_parse(p, "flags:LOS | PASSABLE");
 	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
-	r = parser_parse(p, "info:0:3:0");
+	r = parser_parse(p, "desc:A door that is already open.");
 	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
 	r = parser_parse(p, "desc:A door that is already open.");
 	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
@@ -80,6 +82,41 @@ static int test_missing_header_record0(void *state) {
 	ok;
 }
 
+static int test_code_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "code:XYZZY");
+
+	eq(r, PARSE_ERROR_OUT_OF_BOUNDS);
+	ok;
+}
+
+static int test_code0(void *state) {
+	struct parser *p = (struct parser*) state;
+	enum parser_error r = parser_parse(p, "code:FLOOR");
+	struct feature *f;
+
+	eq(r, PARSE_ERROR_NONE);
+	f = (struct feature*) parser_priv(p);
+	ptreq(f, &f_info[FEAT_FLOOR]);
+	null(f->name);
+	null(f->desc);
+	null(f->mimic);
+	eq(f->fidx, FEAT_FLOOR);
+	eq(f->priority, 0);
+	eq(f->dig, 0);
+	require(flag_is_empty(f->flags, TF_SIZE));
+	eq(f->d_attr, 0);
+	eq(f->d_char, 0);
+	null(f->walk_msg);
+	null(f->run_msg);
+	null(f->hurt_msg);
+	null(f->die_msg);
+	null(f->confused_msg);
+	null(f->look_prefix);
+	null(f->look_in_preposition);
+	ok;
+}
+
 static int test_name0(void *state) {
 	struct parser *p = (struct parser*) state;
 	enum parser_error r = parser_parse(p, "name:Test Feature");
@@ -87,7 +124,7 @@ static int test_name0(void *state) {
 
 	eq(r, PARSE_ERROR_NONE);
 	f = (struct feature*) parser_priv(p);
-	notnull(f);
+	require(f);
 	require(streq(f->name, "Test Feature"));
 	null(f->desc);
 	null(f->mimic);
@@ -110,14 +147,23 @@ static int test_name0(void *state) {
 	ok;
 }
 
+static int test_name_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	/* Specifying a name when there is another one should raise an error. */
+	enum parser_error r = parser_parse(p, "name:Another Name");
+
+	eq(r, PARSE_ERROR_REPEATED_DIRECTIVE);
+	ok;
+}
+
 static int test_graphics0(void *state) {
 	struct parser *p = (struct parser*) state;
-	enum parser_error r = parser_parse(p, "graphics:::Light Green");
+	enum parser_error r = parser_parse(state, "graphics:::red");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
 	f = (struct feature*) parser_priv(p);
-	notnull(f);
+	require(f);
 	eq(f->d_char, L':');
 	eq(f->d_attr, COLOUR_L_GREEN);
 	/* Check that single letter code for color works. */
@@ -155,13 +201,20 @@ static int test_graphics0(void *state) {
 
 
 static int test_mimic0(void *state) {
-	enum parser_error r = parser_parse(state, "mimic:marshmallow");
+	enum parser_error r = parser_parse(state, "mimic:FLOOR");
 	struct feature *f;
 
 	eq(r, PARSE_ERROR_NONE);
 	f = parser_priv(state);
 	require(f);
-	require(streq(f->mimic, "marshmallow"));
+	ptreq(f->mimic, &f_info[FEAT_FLOOR]);
+	ok;
+}
+
+static int test_mimic_bad0(void *state) {
+	enum parser_error r = parser_parse(state, "mimic:XYZZY");
+
+	eq(r, PARSE_ERROR_OUT_OF_BOUNDS);
 	ok;
 }
 
@@ -417,9 +470,13 @@ const char *suite_name = "parse/f-info";
 /* test_missing_header_record0() has to be before test_name0(). */
 struct test tests[] = {
 	{ "missing_header_record0", test_missing_header_record0 },
+	{ "code_bad0", test_code_bad0 },
+	{ "code0", test_code0 },
 	{ "name0", test_name0 },
+	{ "name_bad0", test_name_bad0 },
 	{ "graphics0", test_graphics0 },
 	{ "mimic0", test_mimic0 },
+	{ "mimic_bad0", test_mimic_bad0 },
 	{ "priority0", test_priority0 },
 	{ "flags0", test_flags0 },
 	{ "flags_bad0", test_flags_bad0 },
