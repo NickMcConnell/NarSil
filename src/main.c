NickMcConnell/Beleriand

commit 6d51d768833043dc198a9f8944ca7b324617af78

Avoid crash with -l option when there are no savefiles to list

src/main.c
@@ -276,9 +276,13 @@ static void list_saves(void)
 {
 	savefile_getter g = NULL;
 
-	if (!got_savefile(&g) && !got_savefile_dir(g)) {
+	if (!got_savefile(&g)) {
 		cleanup_savefile_getter(g);
-		quit_fmt("Cannot open savefile directory");
+		if (!got_savefile_dir(g)) {
+			quit_fmt("Cannot open savefile directory");
+		}
+		printf("There are no savefiles you can use.\n");
+		return;
 	}
 
 	printf("Savefiles you can use are:\n");

NickMcConnell/NarSil

commit 78e9bef47fd327f7ecbae50fd1307d5d2fa0c06b

Avoid possible use after free when "-l" option fails
That's a post 1.3.0 regression introduced by 6d51d768833043dc198a9f8944ca7b324617af78 .

src/main.c
@@ -277,8 +277,10 @@ static void list_saves(void)
 	savefile_getter g = NULL;
 
 	if (!got_savefile(&g)) {
+		bool nodir = !got_savefile_dir(g);
+
 		cleanup_savefile_getter(g);
-		if (!got_savefile_dir(g)) {
+		if (nodir) {
 			quit_fmt("Cannot open savefile directory");
 		}
 		printf("There are no savefiles you can use.\n");