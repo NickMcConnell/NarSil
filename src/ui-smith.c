c9484485ba47e315a4e407fd181b99de3150fd7a
Eric Branlund <ebranlund@fastmail.com>
2025-01-23 17:11:14 -0700

    Better mimic Sil 1.3 egos/smithing for base objects with modifiers - 1.3
    In the unmodded game, the noticeable differences are for smithing artifact shovels, mattocks, or shadow cloaks, smithing ego shadow cloaks, or ego shadow cloaks from dungeoon generation or monster drops.  Resolves https://github.com/NickMcConnell/NarSil/issues/851 for the 1.3 branch.  Corrects mismatches with Sil 1.3 for smithed artifact shadow cloaks, shovels, and mattocks that https://github.com/NickMcConnell/NarSil/commit/a7caa79e71d4591b7fe739bce99c609f7282aef6 does not.
    
    Shovels and mattocks now use the tunneling modifier for their base digging bonus rather than the DIG_1 or DIG_2 flags.

--- src/ui-smith.c
+++ src/ui-smith.c
@@ -110,15 +110,19 @@ static bool pval_included = false;
 
 static void include_pval(struct object *obj)
 {
-	int i;
 	if (!pval_included) {
 		object_wipe(smith_obj_backup);
 		object_copy(smith_obj_backup, smith_obj);
 	}
 	if (pval_valid(obj)) {
+		int i;
+
 		obj->pval = pval;
 		for (i = 0; i < OBJ_MOD_MAX; i++) {
-			if (ABS(obj->modifiers[i]) <= 1) obj->modifiers[i] = pval * ABS(obj->modifiers[i]);
+			if (obj->modifiers[i]) {
+				obj->modifiers[i] = (obj->modifiers[i] < 0) ?
+					-pval : pval;
+			}
 		}
 	}
 	pval_included = true;
